63b3810776632a36089c0ebbb37ba68fbf9d74cf
PrathamSingh <prathamsingh96@gmail.com>
2020-04-04 11:52:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: "",
+      searchResult: [],
+      userData: {},
+    };
+  }
 
-export default App;
+  handleChange = event => {
+    event.persist();
+    this.setState({ value: event.target.value }, () =>
+      fetch(`https://api.github.com/search/users?q=${this.state.value}`)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log("data", data);
+          this.setState({ searchResult: data.items || [] });
+        })
+    );
+  };
+
+  handleCardClick = (data) => {
+    fetch(`https://api.github.com/users/${data.login}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log("user data", data);
+      this.setState({ userData: data || {} });
+    })
+  }
+
+  render() {
+    const { value, searchResult } = this.state;
+    return (
+      <>
+        <input
+          className="inputField"
+          name="value"
+          value={value}
+          placeholder="type to begin..."
+          onChange={this.handleChange}
+        />
+        {searchResult.length > 0 && (
+          <div className="cards-container">
+            {searchResult.map(data =>
+              <div
+              className="card"
+              key={data.node_id}
+              onClick={() => this.handleCardClick(data)}
+              >
+                <div className="avatar">
+                  <img src={data.avatar_url} alt="avatar" />
+                </div>
+                <div>
+                  <div>{data.login}</div>
+                  <div>score: {data.score}</div>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </>
+    );
+  }
+}
